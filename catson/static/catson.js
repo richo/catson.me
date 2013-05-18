6f5f67ee0d0f1053ace70802e92ee833a46dd7fe
Richo Healey <richo@psych0tik.net>
2013-05-18 22:28:38 +1000

    fix missing parenthesis

--- catson/static/catson.js
+++ catson/static/catson.js
@@ -6,7 +6,7 @@ function handleFileSelect(evt) {
 
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
-  var img = new Image;
+  var img = new Image();
   img.src = URL.createObjectURL(evt.dataTransfer.files[0]);
   img.onload = function() {
     canvas.height = img.height;
